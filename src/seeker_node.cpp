e95b5d731e4e6ba66394ad3900ffa3ac551b41a4
Chris Evers <chrise92@vt.edu>
2017-10-24 16:25:23 -0400

    enable/disable turtlebot movement

--- src/seeker_node.cpp
+++ src/seeker_node.cpp
@@ -3,7 +3,10 @@
 #include "geometry_msgs/Vector3.h"
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/LaserScan.h"
-#include <sstream>
+#include "std_srvs/SetBool.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
 
 class Seeker
 {
@@ -17,8 +20,9 @@ private:
   // We will be subscribing to the /scan topic to get 2D cross-sectional scans
   ros::Subscriber scan_sub_;
   // We will be using an /enable service to start the scanning
-  //ros::ServiceServer enable_;
-  //ros::ServiceClient client_;
+  ros::ServiceServer service_;
+
+  bool enabled_;
 
 public:
   // ROS node initialization
@@ -30,8 +34,34 @@ public:
     disp_pub_ = nh_.advertise<geometry_msgs::Vector3>("/displacement", 1000);
     // set up the subcriber for the /scan topic
     scan_sub_ = nh_.subscribe<sensor_msgs::LaserScan>("/scan", 1, &Seeker::processLaserScan, this);
-    // up the service
-    //client_ = nh_.serviceClient<seeker::enable>("enable");
+    // setup the service
+    service_ = nh_.advertiseService("/enable", &Seeker::enable, this);
+    ROS_INFO("ready to enable");
+
+    ros::Rate loop_rate(10);
+
+    enabled_ = false;
+  }
+
+  bool enable(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res) {
+    if (req.data) {
+      res.success = true;
+      enabled_ = true;
+      res.message = "enabling seeker";
+      ROS_INFO("seeker enabled");
+    }
+    else {
+      res.success = true;
+      enabled_ = false;
+      res.message = "disabling seeker";
+      ROS_INFO("seeker disabled");
+    }
+
+    return true;
+  }
+
+  bool getEnabled() {
+    return enabled_;
   }
 
   void processLaserScan(const sensor_msgs::LaserScan::ConstPtr& scan) {
@@ -79,11 +109,6 @@ public:
 
     return;
   }
-
-  // Write the /enable service server here
-  void enable() {
-
-  }
 };
 
 /**
@@ -91,64 +116,25 @@ public:
  */
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
   ros::init(argc, argv, "seeker_node");
   
   ros::NodeHandle n;
   Seeker turtlebot(n);
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::Rate loop_rate(10);
+
   while (ros::ok()) {
-    turtlebot.move();
-    ros::spinOnce();
-    loop_rate.sleep();
+    while (!turtlebot.getEnabled()) {
+      ros::spinOnce();
+    }
+
+    ROS_INFO("Turtlebot Activated");
+    while(turtlebot.getEnabled()) {
+      turtlebot.move();
+      ros::spinOnce();
+      loop_rate.sleep();  
+    }    
   }
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  /*ros::Publisher displacement_pub = n.advertise<geometry_msgs::Vector3>("displacement", 1000);
-  ros::Publisher 
-
-  ros::Rate loop_rate(10);
-
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
-  while (ros::ok())
-  {
-    ++count;
-  }
-
 
   return 0;
 }
